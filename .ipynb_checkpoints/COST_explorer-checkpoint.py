76bd76c0daf81f862e8eeaa458dfeedcde846e26
MLOPSopensource <devopsmlopsengineer@gmail.com>
2024-09-28 17:21:31 -0800

    added GANfile

---
+++ .ipynb_checkpoints/COST_explorer-checkpoint.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+print("Data preprocessed successfully!")
+from sklearn.model_selection import train_test_split
+print("Model training in progress...")
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+print("Predictions generated successfully!")
+data = pd.read_csv('e:\\Areas for investment..csv')
+print("Data loaded successfully!")
+print("Model trained successfully!")
